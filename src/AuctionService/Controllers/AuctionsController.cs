4e52ccd0b3aae7aa15ea5272802408fb65ed689d
AnnaaaHus <annaaahus@gmail.com>
2023-12-23 00:03:07 +0200

    End of section 5

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Contracts;
 using MassTransit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -52,13 +53,13 @@ public class AuctionsController : ControllerBase
         return _mapper.Map<AuctionDto>(auction);
 
     }
+    [Authorize]
     [HttpPost]
     public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
     {
         var auction = _mapper.Map<Auction>(auctionDto);
 
-        //TODO add user
-        auction.Seller = "test";
+        auction.Seller = User.Identity.Name;
 
         _context.Auctions.Add(auction);
 
@@ -77,7 +78,7 @@ public class AuctionsController : ControllerBase
 
 
     }
-
+    [Authorize]
     [HttpPut("{id}")]
     public async Task<ActionResult> UpdateAuction(Guid id,UpdateAuctionDto updateAuctionDto)
     {
@@ -86,7 +87,8 @@ public class AuctionsController : ControllerBase
 
         if(auction == null) return NotFound();
 
-        //TODO check seller
+        if (auction.Seller != User.Identity.Name) return Forbid();
+
 
         auction.Item.Make =  updateAuctionDto.Make ?? auction.Item.Make;
         auction.Item.Model =  updateAuctionDto.Model ?? auction.Item.Model;
@@ -103,7 +105,7 @@ public class AuctionsController : ControllerBase
         return BadRequest("Problem saving changes");
 
     }
-
+    [Authorize]
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteAuction(Guid id)
     {
@@ -111,7 +113,7 @@ public class AuctionsController : ControllerBase
 
       if(auction == null) return NotFound();
 
-      //TODO check seller
+      if (auction.Seller != User.Identity.Name) return Forbid();
 
 
        _context.Auctions.Remove(auction);
---
+++ src/IdentityService/Config.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using Duende.IdentityServer.Models;
+
+namespace IdentityService;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("auctionApp","Auction app full access"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "postman",
+                ClientName = "Postman",
+                //ClientSecrets = {new Secret(config["ClientSecret"].Sha256())},
+                AllowedGrantTypes = {GrantType.ResourceOwnerPassword},
+               // RequirePkce = false,
+                RedirectUris = { "/api/auth/callback/id-server"},
+               // AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                ClientSecrets = new[] {new Secret ("NotASecret".Sha256())}
+             //   AccessTokenLifetime = 3600*24*30,
+               // AlwaysIncludeUserClaimsInIdToken = true
+            }
+        };
+}
