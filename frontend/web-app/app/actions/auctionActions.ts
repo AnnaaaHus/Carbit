fdc6e7750290c7b45342cb91dee7bb95d42f8cec
AnnaaaHus <annaaahus@gmail.com>
2024-01-08 14:25:04 +0200

    End of section 13

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { Auction, PagedResult } from "@/types";
+import { Auction, Bid, PagedResult } from "@/types";
 import { getTokenWorkaround } from "./authActions";
 import { fetchWrapper } from "@/lib/fetchWrapper";
 import { FieldValues } from "react-hook-form";
@@ -30,4 +30,10 @@ export async function getDetailedViewData(id: string): Promise<Auction> {
 export async function deleteAuction(id: string) {
     
     return await fetchWrapper.del(`auctions/${id}`);
+}
+export async function getBidsForAuction(id: string): Promise<Bid[]> {
+    return await fetchWrapper.get(`bids/${id}`);
+}
+export async function placeBidForAuction(auctionId: string, amount: number) {
+    return await fetchWrapper.post(`bids?auctionId=${auctionId}&amount=${amount}`, {})
 }
